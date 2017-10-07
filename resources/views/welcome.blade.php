4f571036014a38879e295ff98e2a278b8a1c3466
Roman1488 <romahazvz@gmail.com>
2017-10-07 22:07:02 +0300

    auth system

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,55 +1,34 @@
-@extends('layouts.master')
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('title')
-    Welcome!
-@endsection
+        <title>Welcome!</title>
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
 
-@section('content')
-    @if(count($errors) > 0)
-        <div class="row">
-            <div class="alert alert-danger col-12">
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+            <div class="content">
+                <div class="title m-b-md">
+                    Welcome!
+                </div>
             </div>
         </div>
-    @endif
-    <div class="row">
-        <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
-            <h3>Sign Up</h3>
-            <form action="{{ route('signup') }}" method="post">
-                <div class="form-group {{ $errors->has('email') ? 'has-danger' : '' }}">
-                    <label for="email">Your E-mail</label>
-                    <input class="form-control" type="email" name="email" id="email" value="{{ Request::old('email') }}">
-                </div>
-                <div class="form-group {{ $errors->has('name') ? 'has-danger' : '' }}">
-                    <label for="name">Your Name</label>
-                    <input class="form-control" type="text" name="name" id="name" value="{{ Request::old('name') }}">
-                </div>
-                <div class="form-group {{ $errors->has('password') ? 'has-danger' : '' }}">
-                    <label for="password">Your Password</label>
-                    <input class="form-control" type="password" name="password" id="password">
-                </div>
-                <button class="btn btn-primary" type="submit">Submit</button>
-                <input type="hidden" name="_token" value="{{ Session::token() }}">
-            </form>
-        </div>
-        <div class="col-sm-12 col-md-6	col-lg-6 col-xl-6">
-            <h3>Sign In</h3>
-            <form action="{{ route('signin') }}" method="post">
-                <div class="form-group {{ $errors->has('email') ? 'has-danger' : '' }}">
-                    <label for="email">Your E-mail</label>
-                    <input class="form-control" type="email" name="email" id="email" value="{{ Request::old('email') }}">
-                </div>
-                <div class="form-group {{ $errors->has('password') ? 'has-danger' : '' }}">
-                    <label for="password">Your Password</label>
-                    <input class="form-control" type="password" name="password" id="password">
-                </div>
-                <button class="btn btn-primary" type="submit">Submit</button>
-                <input type="hidden" name="_token" value="{{ Session::token() }}">
-            </form>
-        </div>
-    </div>
-@endsection
+    </body>
+</html>
